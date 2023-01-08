10fe9a8f0ae89667fbd3e4649baf456756f98472
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-01-08 13:30:41 +0530

    Add files via upload
    C Program To Accept Two Integers And Check Whether They Are Equal Or Not.

---
+++ if statements/Assignment-1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a,b;
+	printf("Enter Any Two Numbers: ");
+	scanf("%d%d=",&a,&b);
+	if(a==b)
+	printf("Number1 And Number2 Are Equal ");
+	return 0;
+}
