5da323a6cfc173957af5a981d88dcf17e1b06028
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-03-12 11:06:10 +0530

    Update program-1.c

--- if-else statements/program-1.c
+++ if-else statements/program-1.c
@@ -3,8 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 int main(int argc, char *argv[]) {
 	int a;
 	printf("enter any number: ");
