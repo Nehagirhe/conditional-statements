485a594f55713d34f4ed4dd207267c2f29f0a3d0
Neha Girhe <121210339+Nehagirhe@users.noreply.github.com>
2023-04-20 21:25:20 +0530

    Update program 2.c

--- else-if ladder/program 2.c
+++ else-if ladder/program 2.c
@@ -6,11 +6,15 @@ int main(int argc, char *argv[])
 	int m =-5;
 	if(m>0)
 	printf("The value of n=1");
+
 	else if(m==0)
 	printf("The value of n=0");
+
 	else if(m<0)
 	printf("The value of n=-1");
+
 	else
 	printf("It Is Not In Any Condition");
-	return 0;
+	
+return 0;
 }
